41baf5444de7337e190891d1057c69207dcdbc3e
Howard Choi <hchoi84@gmail.com>
2019-03-11 15:21:10 -0700

    Have player auto move towards target using NavMeshAgent

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform target = default;
+    [SerializeField] private NavMeshAgent myNavMeshAgent = default;
+
+    void Update()
+    {
+        myNavMeshAgent.destination = target.position;
+    }
+}
